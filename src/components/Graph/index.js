13ed7786f3d1a352ce71bb16d0004b322ea52f1e
Henry <henrypye@outlook.com>
2018-03-18 15:53:08 +0000

    Graphs

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 6 */
 import React, { Component } from 'react';
 import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Grid } from 'semantic-ui-react';
 import { GRAPH_URL } from '../../config';
 
 import axios from 'axios';
@@ -34,16 +34,18 @@ export default class Graph extends Component {
     render() {
         return (
             <Segment loading = {this.state.isLoading}>
-                <ResponsiveContainer width="80%" height={600}>
-                    <AreaChart data={this.state.graphData}
-                            margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-                        <XAxis dataKey="time"/>
-                        <YAxis dataKey="alt"/>
-                        <CartesianGrid strokeDasharray="3 3"/>
-                        <Tooltip/>
-                        <Area type='monotone' dataKey='alt' stroke='#8884d8' fill='#8884d8' />
-                    </AreaChart>
-                </ResponsiveContainer>
+                <Grid columns={1}>
+                    <ResponsiveContainer width="80%" height={600}>
+                        <AreaChart data={this.state.graphData}
+                                margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+                            <XAxis dataKey="time"/>
+                            <YAxis />
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <Tooltip/>
+                            <Area type='monotone' dataKey='alt' stroke='#8884d8' fill='#8884d8' />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </Grid>
             </Segment> 
         )
     }
